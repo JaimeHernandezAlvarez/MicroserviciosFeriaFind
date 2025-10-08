c7c9c8cc90fa3ee7667d1e55e0790d8eb7c037e0
Jaime <jai.hernandeza@duocuc.cl>
2025-10-08 12:08:08 -0300

    Inicialización de proyecto

---
+++ Feria/src/main/java/com/service/Feria/FeriaApplication.java
@@ -0,0 +1,13 @@
+package com.service.Feria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeriaApplication.class, args);
+	}
+
+}
---
+++ Feria/src/test/java/com/service/Feria/FeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.Feria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
